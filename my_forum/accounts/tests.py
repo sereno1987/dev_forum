4131749cccfbdc580fa618b3855185ffbefd4e68
sereno <sereno.project@gmail.com>
2019-08-20 00:18:52 +0430

    testcase for sign up (valid/invalid)

--- my_forum/accounts/tests.py
+++ my_forum/accounts/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import signup
@@ -7,7 +9,6 @@ from .views import signup
 
 class SignUp_test(TestCase):
     def setUp(self):
-    #     self.board=acc.objects.create(name="Django", description="django framework")
         self.url = reverse('signup')
         self.response = self.client.get(self.url)
 
@@ -18,8 +19,56 @@ class SignUp_test(TestCase):
         route = resolve("/signup/")
         self.assertEquals(route.func, signup)
 
-    def test_signup_link_back_to_home_page(self):
-        signup_page = reverse("signup")
-        response = self.client.get(signup_page)
-        home_url = reverse('home')
-        self.assertContains(response, 'href="{0}"'.format(home_url))
+    # def test_signup_link_back_to_home_page(self):
+    #     signup_page = reverse("signup")
+    #     response = self.client.get(signup_page)
+    #     home_url = reverse('home')
+    #     self.assertContains(response, 'href="{0}"'.format(home_url))
+
+    def test_csrf(self):
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_contains_form(self):
+        form=self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+
+
+class Successful_signUp_test(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data={
+            'username':'sabahaghighi',
+            'password1':'1234567891',
+            'password2':'123456789',
+        }
+        self.response=self.client.post(url,data)
+        self.home_url=reverse('home')
+
+    def test_redirect(self):
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists)
+
+    def test_authentication(self):
+        response=self.client.get(self.home_url)
+        user=response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+
+
+class UnsSuccessful_signUp_test(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response=self.client.post(url,{})
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_error_form(self):
+        form=self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_failed_user_creation(self):
+        self.assertFalse(User.objects.exists())
